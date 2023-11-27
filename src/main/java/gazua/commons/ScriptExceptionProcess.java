fbe851148a77e6e19607ff4fd94d61a642c9218d
gmkil11 <mkkil4451@gmail.com>
2023-11-28 07:03:26 +0900

    merge를 위한 파일 정리

--- src/main/java/gazua/commons/ScriptExceptionProcess.java
+++ src/main/java/gazua/commons/ScriptExceptionProcess.java
@@ -22,6 +22,6 @@ public interface ScriptExceptionProcess {
 
         model.addAttribute("script", script);
 
-        return "common/_execute_script";
+        return "commons/_execute_script";
     }
 }
